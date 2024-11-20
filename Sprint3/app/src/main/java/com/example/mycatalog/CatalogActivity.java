5e52e6d11d1533deed31899c3c74f28d669113d9
Christian <cperezv24@fpacoruna.afundacion.org>
2024-11-20 10:34:25 +0100

    Empezando proyecto

---
+++ Sprint3/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -0,0 +1,20 @@
+package com.example.mycatalog;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_catalog);
+
+    }
+}
