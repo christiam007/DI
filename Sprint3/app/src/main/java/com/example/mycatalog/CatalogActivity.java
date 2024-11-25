2e1233c4764663e56c68b12bfb891a7227f2f05c
Christian <cperezv24@fpacoruna.afundacion.org>
2024-11-25 12:03:39 +0100

    Tarea completada 2

--- Sprint3/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ Sprint3/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,12 +1,12 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -15,5 +15,13 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
+        Button navigateButton = findViewById(R.id.Navegacionbutton);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
