b729f1242d82942a529273d45579a9f48f14f773
Christian <cperezv24@fpacoruna.afundacion.org>
2024-11-27 23:59:53 +0100

    modificacion

--- Sprint3/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ Sprint3/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -4,9 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
-
-
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -15,10 +14,11 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
-        Button navigateButton = findViewById(R.id.Navegacionbutton);
-        navigateButton.setOnClickListener(new View.OnClickListener() {
+        Button btnLaunchDetail = findViewById(R.id.cataButton);
+        btnLaunchDetail.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+
                 Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
                 startActivity(intent);
             }
--- Sprint3/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ Sprint3/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -18,13 +18,21 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_detail);
-        ImageView productImageView = findViewById(R.id.productImageView);
+
+        // Configurar los insets para la vista principal
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Inicializar la ImageView y cargar la imagen con Glide
+        ImageView imageView = findViewById(R.id.ImageViewId);
         Glide.with(this)
                 .load("https://es.chessbase.com/thumb/2540")
                 .apply(RequestOptions.circleCropTransform()) // Recorte circular
-                .into(productImageView);
-
+                .into(imageView);
     }
 }
