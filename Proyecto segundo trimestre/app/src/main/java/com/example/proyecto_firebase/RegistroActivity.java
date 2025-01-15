b7d715ea99d4f701a5bea128874166a6a60826db
Christian <cperezv24@fpacoruna.afundacion.org>
2025-01-15 16:33:23 +0100

    2# Creacion del Splash y LoginActiviy

--- Proyecto segundo trimestre/app/src/main/java/com/example/proyecto_firebase/RegistroActivity.java
+++ Proyecto segundo trimestre/app/src/main/java/com/example/proyecto_firebase/RegistroActivity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registro);
 
     }
 }
