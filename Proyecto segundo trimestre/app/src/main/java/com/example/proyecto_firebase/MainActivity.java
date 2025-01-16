34612ea8637b065bae62d27a4aa972bf230b6c94
Christian <cperezv24@fpacoruna.afundacion.org>
2025-01-16 01:35:03 +0100

    3# Creacion de Activity de Registro y sincronizacion Registrar y volver al Activity

--- Proyecto segundo trimestre/app/src/main/java/com/example/proyecto_firebase/MainActivity.java
+++ Proyecto segundo trimestre/app/src/main/java/com/example/proyecto_firebase/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.proyecto_firebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView lblRegistrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
+        lblRegistrar = findViewById(R.id.lblRegistrar);
+        lblRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,RegistroActivity.class));
+            }
+        });
     }
 }
--- Proyecto segundo trimestre/app/src/main/java/com/example/proyecto_firebase/RegistroActivity.java
+++ Proyecto segundo trimestre/app/src/main/java/com/example/proyecto_firebase/RegistroActivity.java
@@ -1,6 +1,9 @@
 package com.example.proyecto_firebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegistroActivity extends AppCompatActivity {
 
+    TextView lblLoginR;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_registro);
+
+        lblLoginR = findViewById(R.id.lblLoginR);
+        lblLoginR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistroActivity.this,MainActivity.class));
+            }
+        });
 
     }
 }
