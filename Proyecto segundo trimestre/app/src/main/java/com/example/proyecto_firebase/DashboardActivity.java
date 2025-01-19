9d7ac2a68e43dd41e9d2700281acdb6b43c2ffe7
Christian <cperezv24@fpacoruna.afundacion.org>
2025-01-19 18:59:01 +0100

    4# Creacion del Dashboard y conexion con fire base

--- Proyecto segundo trimestre/app/src/main/java/com/example/proyecto_firebase/DashboardActivity.java
+++ Proyecto segundo trimestre/app/src/main/java/com/example/proyecto_firebase/DashboardActivity.java
@@ -1,14 +1,22 @@
 package com.example.proyecto_firebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class DashboardActivity extends AppCompatActivity {
+
+    Button btnCerrarSesion;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,18 @@ public class DashboardActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_dashboard);
 
+        btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        btnCerrarSesion.setOnClickListener(v -> cierreSesion());
+    }
+
+    private void cierreSesion() {
+
+        firebaseAuth.signOut();
+        startActivity(new Intent(DashboardActivity.this, MainActivity.class));
+        Toast.makeText(this, "Cerraste Sesion Exitosamente", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
--- Proyecto segundo trimestre/app/src/main/java/com/example/proyecto_firebase/RegistroActivity.java
+++ Proyecto segundo trimestre/app/src/main/java/com/example/proyecto_firebase/RegistroActivity.java
@@ -1,31 +1,136 @@
 package com.example.proyecto_firebase;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 
 public class RegistroActivity extends AppCompatActivity {
 
+    EditText etNombre, etApellido, etCorreo, etContrasena, etConfirmContrasena, etTelefono, etDirrecion;
+    Button btnRegistrar;
+
+
     TextView lblLoginR;
+    FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
+
+    String nombre = "", apellido = "", correo = "", contrasena = "", confirmarContrasena = "", telefono = "", direccion = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        //ID DE CONTROLES
+        etNombre = findViewById(R.id.etNombre);
+        etApellido = findViewById(R.id.etApellido);
+        etCorreo = findViewById(R.id.etCorreo);
+        etContrasena = findViewById(R.id.etContrasena);
+        etConfirmContrasena = findViewById(R.id.etConfirmContrasena);
+        etTelefono = findViewById(R.id.etTelefono);
+        etDirrecion = findViewById(R.id.etDirrecion);
+        btnRegistrar = findViewById(R.id.btnRgistrar);
+        lblLoginR = findViewById(R.id.btnRgistrar);
+
+        //Creacion de la Authenticacion
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //Progreso de Dialog se ejecute en Registro Activity
+        progressDialog = new ProgressDialog(RegistroActivity.this);
+        progressDialog.setTitle("Espere porfavor...");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        btnRegistrar.setOnClickListener(v -> validarDatos());
+
+
         lblLoginR = findViewById(R.id.lblLoginR);
-        lblLoginR.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegistroActivity.this,MainActivity.class));
-            }
+        lblLoginR.setOnClickListener(view -> startActivity(new Intent(RegistroActivity.this, MainActivity.class)));
+
+    }
+
+    private void validarDatos() {
+        nombre = etNombre.getText().toString();
+        apellido = etApellido.getText().toString();
+        correo = etCorreo.getText().toString();
+        contrasena = etContrasena.getText().toString();
+        confirmarContrasena = etConfirmContrasena.getText().toString();
+        telefono = etTelefono.getText().toString();
+        direccion = etDirrecion.getText().toString();
+
+        if (TextUtils.isEmpty(nombre)) {
+            Toast.makeText(this, "El campo esta Vacio", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(apellido)) {
+            Toast.makeText(this, "Ingrese Apellido", Toast.LENGTH_SHORT).show();
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+            Toast.makeText(this, "Ingrese Correo Valido", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(contrasena) || contrasena.length()<6) {
+            Toast.makeText(this, "Ingrese Contraseña minimo 6 caracteres", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(confirmarContrasena)) {
+            Toast.makeText(this, "Ingrese Confirmar Contraseña minimo 6 caracteres", Toast.LENGTH_SHORT).show();
+        }else if(!contrasena.equals(confirmarContrasena) || confirmarContrasena.length()<6){
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(telefono)) {
+            Toast.makeText(this, "Ingrese Número de Teléfono", Toast.LENGTH_SHORT).show();
+        } else if (telefono.length() < 9) {
+            Toast.makeText(this, "El número de teléfono debe tener al menos 8 dígitos", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(direccion)) {
+            Toast.makeText(this, "Ingrese Dirección", Toast.LENGTH_SHORT).show();
+        } else if (direccion.length() < 20) {
+            Toast.makeText(this, "La dirección es demasiado corta", Toast.LENGTH_SHORT).show();
+        }else {
+            registrar();
+        }
+
+    }
+
+    private void registrar() {
+        progressDialog.setMessage("Registrando al Usuario");
+        progressDialog.dismiss();
+
+        firebaseAuth.createUserWithEmailAndPassword(correo,contrasena).
+                addOnSuccessListener(authResult -> guardarUsuario()).addOnFailureListener(e -> Toast.makeText(RegistroActivity.this,"Ocurrio un problema, revisa los campos", Toast.LENGTH_SHORT).show());
+
+    }
+
+    private void guardarUsuario() {
+        progressDialog.setMessage("Guardando Informacion");
+        progressDialog.dismiss();
+
+        String uid = firebaseAuth.getUid();
+        HashMap<String , String> datosUsuario = new HashMap<>();
+        datosUsuario.put("uid", uid);
+        datosUsuario.put("nombre", nombre);
+        datosUsuario.put("apellido",apellido);
+        datosUsuario.put("correo", correo);
+        datosUsuario.put("contrasena",contrasena);
+        datosUsuario.put("telefono", telefono);
+        datosUsuario.put("direccion", direccion);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Usuarios");
+        databaseReference.child(uid).setValue(datosUsuario).addOnSuccessListener(unused -> {
+            progressDialog.dismiss();
+            Toast.makeText(RegistroActivity.this,"Usuario Creado con Existo", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(RegistroActivity.this,DashboardActivity.class));
+            finish();
+        }).addOnFailureListener(e -> {
+            progressDialog.dismiss();
+            Toast.makeText(RegistroActivity.this,"Ocurrio un problema"+e.getMessage(), Toast.LENGTH_SHORT).show();
         });
 
     }
